3204ab452ab8fee7461ea6da781840a065b566b8
unknown <nmiolane@COUSINOT.inria.fr>
2014-07-25 15:46:04 +0200

    2nd edit

--- expL.m
+++ expL.m
@@ -1,8 +1,8 @@
 % start: TODO
 % What the function does: Riemannian exponential with Left (L)-invariant metric
 % clearer function name ? 
-% Inputs description:
-% Outputs description:
+% Inputs description: f shooting point, v shooted vector
+% Outputs description: ff point reached after shooting
 % end:  TODO
 
 function [ff]=expL(f,v)
